34b3d8aa236cbc504df86eebcd08b254c63b5761
mary-cleaton <m.a.m.cleaton@gmail.com>
2024-07-28 14:15:28 +0100

    feat: wrote `join_lookup` and its unit test

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,7 +3,7 @@ import pytest
 
 
 @pytest.fixture(scope="function")
-def get_input_dataset():
+def get_input_dataset_1():
     return pd.DataFrame(
         {
             "forename": ["Peter", "Aisha", "Colin", "Lucy-Anne"],
@@ -33,5 +33,42 @@ def get_output_dataset_2():
         {
             "forename": ["Peter", "Aisha", "Colin", "Lucy Anne"],
             "surname": ["Gardiner", "Al Fayed", "Quentin Jr ", "Griffiths"],
+        }
+    )
+
+
+@pytest.fixture(scope="function")
+def get_input_dataset_2():
+    return pd.DataFrame(
+        {
+            "creatures": ["cat", "dog", "parrot", "cuttlefish", "guppy"],
+        }
+    )
+
+
+@pytest.fixture(scope="function")
+def get_input_dataset_3():
+    return pd.DataFrame(
+        {
+            "common_name": ["cat", "dog", "parrot", "squid", "guppy"],
+            "animal_type": [
+                "mammal",
+                "mammal",
+                "bird",
+                "invertebrate",
+                "fish",
+            ],
+            "vertebrate": [True, True, True, False, True],
+        }
+    )
+
+
+@pytest.fixture(scope="function")
+def get_output_dataset_3():
+    return pd.DataFrame(
+        {
+            "creatures": ["cat", "dog", "parrot", "cuttlefish", "guppy"],
+            "animal_type": ["mammal", "mammal", "bird", None, "fish"],
+            "vertebrate": [True, True, True, None, True],
         }
     )
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,10 +1,72 @@
 import os
 from contextlib import nullcontext as does_not_raise
 
+import pandas as pd
 import pytest
 import yaml
 
 from src import utils
+
+
+@pytest.mark.parametrize(
+    "params, test_input_df_fixture, test_input_lookup_fixture, "
+    + "expected_output_fixture, expected_context",
+    [
+        (
+            {
+                "df": None,
+                "lookup": None,
+                "df_col": "creatures",
+                "lookup_col": "common_name",
+            },
+            "get_input_dataset_2",
+            "get_input_dataset_3",
+            "get_output_dataset_3",
+            does_not_raise(),
+        ),
+        (
+            {
+                "df": None,
+                "lookup": None,
+                "df_col": "not_a_df_col",
+                "lookup_col": "common_name",
+            },
+            "get_input_dataset_2",
+            "get_input_dataset_3",
+            "get_null",
+            pytest.raises(ValueError),
+        ),
+        (
+            {
+                "df": None,
+                "lookup": None,
+                "df_col": "creatures",
+                "lookup_col": "not_a_lookup_col",
+            },
+            "get_input_dataset_2",
+            "get_input_dataset_3",
+            "get_null",
+            pytest.raises(ValueError),
+        ),
+    ],
+)
+def test_join_lookup(
+    request,
+    params,
+    test_input_df_fixture,
+    test_input_lookup_fixture,
+    expected_output_fixture,
+    expected_context,
+):
+    test_input_df = request.getfixturevalue(test_input_df_fixture)
+    test_input_lookup = request.getfixturevalue(test_input_lookup_fixture)
+    expected_output = request.getfixturevalue(expected_output_fixture)
+    params["df"] = test_input_df
+    params["lookup"] = test_input_lookup
+    with expected_context:
+        pd.testing.assert_frame_equal(
+            utils.join_lookup(**params), expected_output, check_dtype=False
+        )
 
 
 @pytest.mark.parametrize(
@@ -40,19 +102,19 @@ def test_read_yaml(
     [
         (
             {"df": None, "col": "forename"},
-            "get_input_dataset",
+            "get_input_dataset_1",
             "get_output_dataset_2",
             does_not_raise(),
         ),
         (
             {"df": None, "col": "surname"},
-            "get_input_dataset",
+            "get_input_dataset_1",
             "get_output_dataset_2",
             does_not_raise(),
         ),
         (
             {"df": None, "col": "not_a_df_col"},
-            "get_input_dataset",
+            "get_input_dataset_1",
             "get_null",
             pytest.raises(ValueError),
         ),
@@ -79,19 +141,19 @@ def test_remove_symbols_in_string_col(
     [
         (
             {"df": None, "col": "forename"},
-            "get_input_dataset",
+            "get_input_dataset_1",
             "get_output_dataset_1",
             does_not_raise(),
         ),
         (
             {"df": None, "col": "surname"},
-            "get_input_dataset",
+            "get_input_dataset_1",
             "get_output_dataset_1",
             does_not_raise(),
         ),
         (
             {"df": None, "col": "not_a_df_col"},
-            "get_input_dataset",
+            "get_input_dataset_1",
             "get_null",
             pytest.raises(ValueError),
         ),
