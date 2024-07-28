2e6803f95c20d0b8dfd352b925da1d1183551c1f
mary-cleaton <m.a.m.cleaton@gmail.com>
2024-07-28 12:34:57 +0100

    feat: wrote `main.py` to call functions; updated helper files updated `.gitignore` to ignore `output` folder updated `config.yaml` to call configs slightly differently updated `run.py` to call pipeline from `main.py` updated `mock_data.csv` to include an instance of a symbol in the string columns being cleaned updated `pyproject.toml` to specify pandas version

--- run.py
+++ run.py
@@ -1,26 +1,34 @@
-""" """
+"""
+LANGUAGE: Python
+WHAT IT DOES:
+* reads configs from yaml file
+* reads .csv file
+* applies standard cleaning
+* writes outputs to .csv file
 
-from src import logging_handler, utils
+AUTHOR: Mary Cleaton
+DATE: 28/07/2024
+REF: UKHSA Data Engineer job application
+"""
 
-configs = utils.read_yaml("input/config.yaml")
-print(configs["TABLE_NAME"])
-print(type(configs["TABLE_NAME"]))
+from src import logging_handler, main, utils
+
+config_filepath = "input/config.yaml"
+table_name = "test"
 
 
-def run_pipeline(configs):
-    table_name = configs["TABLE_NAME"]
+def run(config_filepath, table_name):
+    configs = utils.read_yaml(config_filepath)
+
     logger = logging_handler.Logger(table_name).get_logger()
     logger.info(f"Commenced {table_name} processing")
 
     try:
-        # Pipeline here
-        x = "HELLO WORLD"
-        print(5 / x)
-
+        main.pipeline(**configs)
         logger.info(f"Successfully processed {table_name}")
     except Exception as e:
         logger.exception(e)
 
 
 if __name__ == "__main__":
-    run_pipeline(configs)
+    run(config_filepath, table_name)
---
+++ src/main.py
@@ -0,0 +1,34 @@
+from typing import Dict, List
+
+import pandas as pd
+
+from src import utils
+
+
+def pipeline(
+    INPUT_FILE_NAME: str,
+    OUTPUT_FILE_NAME: str,
+    INPUT_FILEPATH: str,
+    OUTPUT_FILEPATH: str,
+    STRING_COLUMNS: List,
+    COLUMN_RENAME_DICT: Dict,
+):
+    """ """
+    df = pd.read_csv(
+        INPUT_FILEPATH + INPUT_FILE_NAME,
+        index_col=0,
+        header=0,
+    )
+
+    for i in STRING_COLUMNS:
+        df = utils.remove_symbols_in_string_col(
+            df=df,
+            col=i,
+        )
+        df = utils.set_string_col_to_upper(
+            df=df,
+            col=i,
+        )
+
+    df.to_csv(OUTPUT_FILEPATH + OUTPUT_FILE_NAME)
+    return df
