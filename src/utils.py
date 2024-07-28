315acb3d86f1b392f1002f2a81716e3517473206
mary-cleaton <m.a.m.cleaton@gmail.com>
2024-07-28 09:34:36 +0100

    feat: wrote `remove_symbols_in_string_col` and its unit test

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,34 @@
 import pandas as pd
 
 
+def remove_symbols_in_string_col(df: pd.DataFrame, col: str) -> pd.DataFrame:
+    """
+    Converts the values of a string column in a Pandas dataframe to remove all
+        characters except alphanumeric and spaces.
+
+    Args:
+        df (pd.DataFrame): The Pandas dataframe containing the string column.
+        col (str): The name of the string column to be converted.
+
+    Returns:
+        df_updated (pd.DataFrame): The Pandas dataframe df with the values of
+            the string column col converted to remove all characters except
+            alphanumeric and spaces.
+
+    Raises:
+        ValueError: if col is not a column of df.
+    """
+    if col not in df.columns:
+        raise ValueError("col must be a column of df")
+    df_updated = df
+    df_updated[col] = df_updated[col].str.replace(
+        r"[^0-9a-zA-Z ]+",
+        r" ",
+        regex=True,
+    )
+    return df_updated
+
+
 def set_string_col_to_upper(df: pd.DataFrame, col: str) -> pd.DataFrame:
     """
     Converts the values of a string column in a Pandas dataframe to uppercase.
