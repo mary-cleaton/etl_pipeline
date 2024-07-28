f97b316a7420ecea3b51b491ad15ee213a969562
mary-cleaton <m.a.m.cleaton@gmail.com>
2024-07-28 16:47:31 +0100

    docs: added docstring for `pipeline()`

--- src/main.py
+++ src/main.py
@@ -15,7 +15,38 @@ def pipeline(
     COLUMN_RENAME_DICT: Dict[str, str],
     SOC_LOOKUP_COLUMNS: Dict[str, str],
 ):
-    """ """
+    """
+    Runs the pipeline as follows:
+        - Reads input dataframe and SOC lookup .csv files to Pandas dataframes.
+        - Performs basic string cleaning.
+        - Joins the SOC lookup onto the dataframe.
+        - Hashes specified columns.
+        - Writes the transformed dataframe to the specified output filepath and
+          filename.
+
+    Args:
+        INPUT_FILE_NAME (str): The name of the file containing the dataset to
+            be processed.
+        OUTPUT_FILE_NAME (str): The name of the file that the transformed
+            dataset will be written to.
+        SOC_LOOKUP_FILE_NAME (str): The name of the file containing the SOC
+            lookup.
+        INPUT_FILEPATH (str): The filepath of the input folder, containing the
+            input file and the SOC lookup.
+        OUTPUT_FILEPATH (str): The filepath of the output folder, where the
+            output file will be written to.
+        STRING_COLUMNS (List[str]): The names of the string columns
+            that string column cleaning will be applied to.
+        COLUMN_RENAME_DICT (Dict[str, str]): The columns that will be renamed
+            where dictionary keys are the old names and values are the new
+            names.
+        SOC_LOOKUP_COLUMNS (Dict[str, str]): The columns to use for joining on
+            the SOC lookup. The keys must be "df_col" and "lookup_col" and the
+            values are the names that those columns hold.
+
+    Returns:
+       df (pd.DataFrame): The transformed dataframe.
+    """
     df = pd.read_csv(
         INPUT_FILEPATH + INPUT_FILE_NAME,
         index_col=0,
